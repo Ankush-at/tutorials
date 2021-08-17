30908752bb980ee44f42482c66d729a50415d1af
ankush-at <ankushthakre1995@gmail.com>
2021-08-17 14:19:50 +0530

    first push

---
+++ tutorial/Shashank.py
@@ -0,0 +1,92 @@
+"""
+WSGI config for tutorial project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutorial.settings')
+
+application = get_wsgi_application()
+
+
+# WSGI congig for Stack 
+Import Docker hub
+from api.py setdefault
+WSGI config for tutorial project .
+
+From python:3-onbuild
+COPY./usrsrc/app
+CMD ["python","api.py"]
+>>> import pdb
+>>> import mymodule
+>>> pdb.run('mymodule.test()')
+> <string>(0)?()
+(Pdb) continue
+> <string>(1)?()
+(Pdb) continue
+NameError: 'spam'
+
+NameError
+
+> <string>(1)?()
+(Pdb)
+From api.py setdefault
+WSGI config for tutorial project.
+From python:3-onbuild copy
+import pdb
+copy./urssrc/application
+CMD["NameError:"]
+(Pdb) continue
+> <string>(1)
+#Docker (This coment in this statement is invisible)
+"import"
+<Html> 
+
+<head>
+<title>
+My website
+<title>
+
+<body>
+
+
+
+class Product(Naviagtion):
+	dref get(self)
+	return
+	'product': [drop down]
+	           'navigation'
+	           'contact us'
+	           }api.run(host='9.9.9.9',port=580, debug=false)
+	           flask ==.12
+</body>
+</head>
+<Dropdown>
+
+lass Product(Naviagtion):
+	dref get(self)
+	return
+	'product': [drop down]
+	           'navigation'
+	           'contact us'
+	           }api.run(host='9.9.9.9',port=580, debug=false)
+	           flask ==.12
+
+<H1> </H1>
+</dropdown>
+<Artificial intelligence >
+<'Context'>
+<api.run()Host=00000>
+
+
+
+
+
+</html>
